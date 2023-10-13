5e373b48fa2188f00de689c27630d92c2aca4733
Sylvebois <thomasghaye@hotmail.com>
2023-10-13 12:51:55 +0000

    Added threads and response time log

--- ia.js
+++ ia.js
@@ -1,25 +1,28 @@
-import {fileURLToPath} from "url";
-import path from "path";
-import {LlamaModel, LlamaContext, LlamaChatSession} from "node-llama-cpp";
+import { LlamaModel, LlamaContext, LlamaChatSession } from 'node-llama-cpp';
+import { config } from 'dotenv';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+config();
 
-const model = new LlamaModel({
-    modelPath: path.join(__dirname, "llm", "models", "vigogne-2-7b-instruct.Q4_K_M.gguf")
-});
-const context = new LlamaContext({model});
-const session = new LlamaChatSession({context});
+const model = new LlamaModel({ modelPath: process.env.MODEL_PATH });
+const context = new LlamaContext({ model, threads: 14 });
+const session = new LlamaChatSession({ context });
 
+const q1 = 'Bonjour, comment vas-tu?';
+console.log('User: ' + q1);
 
-const q1 = "Bonjour, comment vas-tu?";
-console.log("User: " + q1);
-
+let start = Date.now();
 const a1 = await session.prompt(q1);
-console.log("AI: " + a1);
+console.log('AI: ' + a1);
+let end = Date.now();
 
+console.log(`Première réponse en ${end-start}ms`)
 
-const q2 = "Summerize what you said";
-console.log("User: " + q2);
+const q2 = 'Summerize what you said';
+console.log('User: ' + q2);
 
+start = Date.now();
 const a2 = await session.prompt(q2);
-console.log("AI: " + a2);
+console.log('AI: ' + a2);
+end = Date.now();
+
+console.log(`Seconde réponse en ${end-start}ms`)
