097a18c8fd589b2dc3a2b733cbba6fd83c81a31b
Sylvebois <thomasghaye@hotmail.com>
2023-10-13 14:18:20 +0000

    Testing response time

--- ia.js
+++ ia.js
@@ -3,11 +3,24 @@ import { config } from 'dotenv';
 
 config();
 
-const model = new LlamaModel({ modelPath: process.env.MODEL_PATH });
-const context = new LlamaContext({ model, threads: 14 });
-const session = new LlamaChatSession({ context });
+const model = new LlamaModel({ modelPath: process.env.MODEL_PATH, useMmap: false });
+const context = new LlamaContext({ model, batchSize: 4096, threads: 14 });
+const session = new LlamaChatSession({ 
+  temperature:0,
+  context,
+ /* systemPrompt: `
+You are Plombo, a French speaking, knowledgeable and helpful AI assistant who fulfills any request with detail and precision. You always use a polite language (an example is the use of the formal form of address instead of the informal one) and, if you do not know the anwser, you just say "Désolé, je ne trouve pas cette information, un de mes collègues pourrait peut-être vous aider ..."
+You are specialized in heating systems (radiators, boiler, floor heating ...), plumbing and sanitary products (baths, showers, toilets, bathroom meubels ...). 
+You are working for the company Sanidel and you have access to many catalogs and Excel listings to extend your knowledge.
+The user is a customer who uses Plombo to recieve informations (price, dimensions, power and other technical data ...) about a large variety of products (heating systems, plumbing, bathroom, toilet, faucet, sink ...).
+Sanidel est une société familiale belge spécialisée dans les systèmes de chauffage et la plomberie mais également dans l'aménagement de salles de bains (baingnoires, bacs de douche, parois de douches, meubles, wc, lavabos, lave-mains, robinetterie) et de cuisines (meubles, plan de travail, électroménagers, éviers, robinetterie).
+Sanidel fait de la vente en gros et au détail. La société vend essentiellement à des installateurs professionnels mais peut également vendre aux particuliers.
+La société a été fondée en 1969 par Charles Delvaux. Le dirigeant actuel est Luc Delvaux, fils du fondateur.
+Le premier établissement se situait à Marloie. Le magasin a ensuite déménagé vers Marche-en-Famenne, à proximité de la nationale 4. Il existe deux autres magasins, un à Jambes (où se trouve le siège social) et un à Liège. `
+*/
+ });
 
-const q1 = 'Bonjour, comment vas-tu?';
+const q1 = `Bonjour, comment t'appelles-tu ?`;
 console.log('User: ' + q1);
 
 let start = Date.now();
@@ -17,12 +30,22 @@ let end = Date.now();
 
 console.log(`Première réponse en ${end-start}ms`)
 
-const q2 = 'Summerize what you said';
+const q2 = `Que peux-tu me dire sur la société Sanidel ?`;
 console.log('User: ' + q2);
 
 start = Date.now();
 const a2 = await session.prompt(q2);
 console.log('AI: ' + a2);
+end = Date.now();
+
+console.log(`Seconde réponse en ${end-start}ms`)
+
+const q3 = `Qu'est-ce que le thermosiphon ?`;
+console.log('User: ' + q3);
+
+start = Date.now();
+const a3 = await session.prompt(q3);
+console.log('AI: ' + a3);
 end = Date.now();
 
 console.log(`Seconde réponse en ${end-start}ms`)
